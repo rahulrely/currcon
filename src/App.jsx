63809d98fb73acd2434bd410fa20d66661d394e5
Rahul Singh <rahul274301@gmail.com>
2025-07-25 13:15:39 +0530

    fix: correct currency change handler to update 'from' state

--- src/App.jsx
+++ src/App.jsx
@@ -43,7 +43,7 @@ function App() {
                               label="From"
                               amount={amount}
                               currencyOption={option}
-                              onCurrencyChange={(currency)=> setAmount(amount)}
+                              onCurrencyChange={(currency)=> setFrom(currency)}
                               selectCurrency={from}
                               onAmountChange={(amount)=> setAmount(amount)}
                           />
